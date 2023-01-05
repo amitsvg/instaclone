a061b415b8de227e4082e80a983c867ae2179b01
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2023-01-05 15:48:33 +0530

    path static

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const connectDB = async () => {
 if(process.env.NODE_ENV ==="production"){
     app.use(express.static(path.join("frontend/build")));
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"),
+      res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"),
       function (err){
           res.status(500).send(err)
         }
