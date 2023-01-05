e53f05fda4924b232d7470cae909a1f613330a9a
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2023-01-05 15:40:26 +0530

    good

--- server.js
+++ server.js
@@ -39,17 +39,16 @@ const connectDB = async () => {
 //     console.log("not connected to mongodb");
 // })
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(_dirname, "frontend/build")));
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join("frontend/build")));
     app.get("*", (req, res) => {
-        res.sendFile(
-            path.join(__dirname, "frontend/build/index.html"),
-            function (err) {
-                res.status(500).send(err);
-            }
-        )
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"),
+      );
+      function (err){
+        res.status(500).send(err)
+      }
     });
-}
+  }
 
 connectDB().then(() => {
 
