850ff4c1a1feea2649effd1a81723b2eaab83c23
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2023-01-05 15:03:28 +0530

    build created and directories changed, backend isin root folder

--- server.js
+++ server.js
@@ -30,14 +30,13 @@ mongoose.connection.on("error", () => {
     console.log("not connected to mongodb");
 })
 
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join("frontend/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    });
+}
 
 app.listen(PORT, () =>{
     console.log(`server si running on ${PORT}`);
 })
-
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static(path.join("frontend/build")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-    });
-  }
